3ec08173bb22bc9420ea89c0e9c31c937135a3a7
abiramibharanidharan <abiramibharanidharan@gmail.com>
2017-09-18 09:01:59 +0530

    commit

---
+++ src/app/app.logincomponent.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.logincomponent.html',
+  styleUrls: ['./app.logincomponent.css']
+})
+export class LoginComponent {
+  title = 'Welcome to Login';
+}
